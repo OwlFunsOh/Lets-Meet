d8264e54a306fe8018d6a883e3338ccc84877a82
Alfonso Martinez <amartin6@umbc.edu>
2024-09-25 17:32:42 -0400

    deleted firebase stuff

--- login-functions.js
+++ login-functions.js
@@ -1,21 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-apiKey: process.env.apiKey,
-authDomain: process.env.authDomain,
-projectId: process.env.projectId,
-storageBucket: process.env.storageBucket,
-messagingSenderId: process.env.messagingSenderId,
-appId: process.env.appId
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
 //Sign up page when you hit Sign up button
 document.querySelector("#show-register").addEventListener("click", () => {
     showRegistration();
