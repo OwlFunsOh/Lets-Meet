36b1e2ccdb5f6a1855a7803876b26b9c871a5417
Alfonso Martinez <amartin6@umbc.edu>
2024-10-10 17:13:00 -0400

    Login functionality minus secret keys

--- login-functions.js
+++ login-functions.js
@@ -8,12 +8,7 @@ import { getAuth, createUserWithEmailAndPassword } from 'https://www.gstatic.com
 // Your web app's Firebase configuration
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDohRYNwOQsh8YLeevAHeGi57BsPDC311E",
-    authDomain: "lets-meet-47a8c.firebaseapp.com",
-    projectId: "lets-meet-47a8c",
-    storageBucket: "lets-meet-47a8c.appspot.com",
-    messagingSenderId: "847905556985",
-    appId: "1:847905556985:web:df8375a05e80ed9c95d528"
+    //insert secret stuff here
 };
 
 // Initialize Firebase
