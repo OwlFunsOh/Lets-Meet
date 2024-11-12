59ae816e2f914995dffbe9600d865769b4cafbd0
pookizzz <78450160+pookikitty@users.noreply.github.com>
2024-11-11 20:26:54 -0500

    file system organized

---
+++ src/homepage/homepage-functions.js
@@ -0,0 +1,9 @@
+// Select the button by its ID
+const logoutButton = document.getElementById('logout');
+
+// Add a click event listener
+logoutButton.addEventListener('click', () => {
+  alert('Button clicked!');
+  // You can add your logout logic here
+  console.log('Logout button clicked.');
+});
