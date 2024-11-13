64e76d93989c7098c1de3c3538519bd40cb09102
Alfonso Martinez <amartin6@umbc.edu>
2024-11-13 18:29:32 -0500

    logout functionality

--- src/homepage/homepage-functions.js
+++ src/homepage/homepage-functions.js
@@ -1,8 +1,17 @@
+import { auth } from '/src/firebase.js';
+
 // Select the button by its ID
 const logoutButton = document.getElementById('logout');
 
 // Add a click event listener
 logoutButton.addEventListener('click', () => {
   // You can add your logout logic here
-  window.location.href = "/";
+  auth.signOut().then(() => {
+    //sign out successful
+    window.location.href = "/";
+  }).catch((error) => {
+    //an error happened
+    const errorMessage = error.message;
+    alert(errorMessage);
+  });
 });
