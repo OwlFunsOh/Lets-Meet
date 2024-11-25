ac261a327d9b39c591a8e1ddc55fb9165f9725f0
pookizzz <78450160+pookikitty@users.noreply.github.com>
2024-11-25 14:15:20 -0500

    Group UI WIP

--- src/grouppage/grouppage-functions.js
+++ src/grouppage/grouppage-functions.js
@@ -2,4 +2,16 @@ import { auth, db } from '/src/firebase.js';
 import { doc, collection, setDoc, addDoc, getDoc } from 'https://www.gstatic.com/firebasejs/10.14.1/firebase-firestore.js'
 import { getAuth, onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/10.14.1/firebase-auth.js'
 
-// hi 
+//function to join a group
+function signIn() {
+  const groupcode = document.querySelector("#group-code").value
+
+}
+
+// Return to homepage
+async function back_button() {
+  window.location.href = "/src/homepage/homepage.html";
+}
+document.querySelector("#show-back").addEventListener("click", () => {
+  back_button();
+})
