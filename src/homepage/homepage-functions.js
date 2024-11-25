98d654199e318ed2e63315d349b3db22ac7dbc62
Alfonso Martinez <amartin6@umbc.edu>
2024-11-25 11:43:45 -0500

    better calendar

--- src/homepage/homepage-functions.js
+++ src/homepage/homepage-functions.js
@@ -20,48 +20,9 @@ logoutButton.addEventListener('click', () => {
 
 document.addEventListener("DOMContentLoaded", () => {
   const calendarGrid = document.getElementById("calendar-grid");
-
+});
   // Alfonso: add joined group buttons, use #group-button css
 
-
-
-  // Specify the number of rows and columns
-  const rows = 8; // Number of rows
-  const cols = 7; // Number of columns
-
-  // Populate the grid dynamically
-  for (let i = 0; i < rows * cols; i++) {
-    const gridItem = document.createElement("div");
-    gridItem.classList.add("grid-item"); // Add class for styling
-    switch (i) {
-      case 0:
-        gridItem.textContent = "Sunday"
-        break;
-      case 1:
-        gridItem.textContent = "Monday";
-        break;
-      case 2:
-        gridItem.textContent = "Tuesday";
-        break;
-      case 3:
-        gridItem.textContent = "Wednesday";
-        break;
-      case 4:
-        gridItem.textContent = "Thursday";
-        break;
-      case 5:
-        gridItem.textContent = "Friday";
-        break;
-      case 6:
-        gridItem.textContent = "Saturday";
-        break;
-      default:
-        gridItem.textContent = `Item ${i + 1}`; // Optional: Add text to each cell
-        break;
-    }
-    calendarGrid.appendChild(gridItem); // Append the cell to the grid container
-  }
-});
 
 //adding greeting
 onAuthStateChanged(auth, (user) => {
