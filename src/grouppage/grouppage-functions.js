75ac4939637c8a749144ce09deed39acef83eafb
Gabriel <gsternb1@umbc.edu>
2024-12-01 23:42:08 -0500

    Grouppage functions how have if statements, but not joins

--- src/grouppage/grouppage-functions.js
+++ src/grouppage/grouppage-functions.js
@@ -6,7 +6,11 @@ import { getAuth, onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/
 function joinGroup() {
   const groupcode = document.querySelector("#group-code").value
 
-  // program
+  if (groupcode.exists()){
+    //join group
+  } else{
+    console.log("Group code does not exist");
+  }
 }
 document.querySelector("#join-button").addEventListener("click", () => {
   joinGroup();
@@ -14,8 +18,15 @@ document.querySelector("#join-button").addEventListener("click", () => {
 
 // Create a group
 async function createGroup() {
-   // program
-  
+   const groupname = document.querySelector("#group-title").value
+  if (groupname.length >= 3){
+    console.log("Group name is too short");
+  }else if (groupnamename.length <= 20) {
+    console.log("Group name is too long");
+  }else{
+    //create group
+    //join group
+  }
 }
 document.querySelector("#create-button").addEventListener("click", () => {
   createGroup();
